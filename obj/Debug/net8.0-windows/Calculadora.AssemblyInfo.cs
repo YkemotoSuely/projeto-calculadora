1ae945a08e6266b5e763f481044f3eacf15ac57e
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-07-30 21:57:20 -0300

    Correção de nbomes de arquivos

--- obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Caclculadora")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+233c08a63de68521329a40cc6d4427a885f3bd59")]
-[assembly: System.Reflection.AssemblyProductAttribute("Caclculadora")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Caclculadora")]
+[assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
