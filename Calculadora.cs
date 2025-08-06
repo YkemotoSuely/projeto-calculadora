fccaba4f7f53e88b30ae29fe6969b597ef927581
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-08-05 21:16:03 -0300

    Adiciona o cálculo do botão Subtrair

--- Calculadora.cs
+++ Calculadora.cs
@@ -8,12 +8,25 @@ namespace Caclculadora
         }
 
         private void btnSomar_Click(object sender, EventArgs e)
-        {
+
+        {   //Verifica se os campos de entrada estão preenchidos
             double numero1 = Convert.ToDouble(txtNumero1.Text);
             double numero2 = Convert.ToDouble(txtNumero2.Text);
 
+            // Realiza a soma dos números e exibe o resultado
             lblResultado.Text = (numero1 + numero2).ToString();
 
+
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+       
+        {   //Verifica se os campos de entrada estão preenchidos
+            double numero1 = Convert.ToDouble(txtNumero1.Text);
+            double numero2 = Convert.ToDouble(txtNumero2.Text);
+
+            // Realiza a subtração dos números e exibe o resultado
+            lblResultado.Text = (numero1 - numero2).ToString();
         }
     }
 }
