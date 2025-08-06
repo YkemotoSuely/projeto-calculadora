6544b5d98e95822ece00243e0347aa6646df4247
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-08-05 21:25:14 -0300

    Adiciona o calculo do botão Dividir

--- Calculadora.cs
+++ Calculadora.cs
@@ -38,5 +38,13 @@ namespace Caclculadora
             //Realiza a multiplicação dos números e exibe o resultado
             lblResultado.Text = (numero1 * numero2).ToString();
         }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtNumero1.Text);
+            double numero2 = Convert.ToDouble(txtNumero2.Text);
+
+            lblResultado.Text = (numero1 / numero2).ToString();
+        }
     }
 }
